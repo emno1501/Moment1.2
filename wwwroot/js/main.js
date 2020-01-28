a71046f9ae636df281585cdccef7c5d2e4dee923
Emma Norgren <emno1501@student.miun.se>
2020-01-28 19:36:55 +0100

    Färdig ASP.NET Core webbapplikation, första commit.

---
+++ wwwroot/js/main.js
@@ -0,0 +1,9 @@
+﻿//Ändra färg på header när man scrollar nedåt
+window.onscroll = function () {
+    var header = document.getElementById('header');
+    if (window.scrollY > 100) {
+        header.classList.add('headerBackground');
+    } else {
+        header.classList.remove('headerBackground');
+    }
+}
